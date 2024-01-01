7c878ffec7e20752c33a87bfd88bdad6b0f643b0
A-NV <anthony.neav020@gmail.com>
2024-01-01 18:06:35 +1100

    Template Farbic mod

---
+++ src/main/java/net/zeicy1/mod/TemplateModClient.java
@@ -0,0 +1,12 @@
+package net.zeicy1.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TemplateModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+        
+    }
+    
+}
